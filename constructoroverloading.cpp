d0dda66c0e740f50e2675758ea24190987d57517
iizaghz <sukmahawaiza@gmail.com>
2025-05-13 19:21:48 +0700

    add constructor over loading untuk mhs1,mhs2, dan mhs3

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -27,3 +27,9 @@ void mahasiswa::cetak(){
     cout << endl << "Nim = " <<nim << endl;
     cout << "Nama =" << nama << endl;
 }
+
+int main(){
+    mahasiswa mhs1(102030); //hanya nim
+    mahasiswa mhs2("Andi"); //hanya nama
+     mahasiswa mhs3(010203, "Santi"); //NIM dan nama
+}
