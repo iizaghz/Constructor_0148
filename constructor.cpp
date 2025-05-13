f3a0365c1e2e1fd5a9f71c079feded3ce07ef5bd
iizaghz <sukmahawaiza@gmail.com>
2025-05-13 18:50:25 +0700

    add objek mahasiswa mhs

--- constructor.cpp
+++ constructor.cpp
@@ -9,3 +9,8 @@ class mahasiswa{
 mahasiswa::mahasiswa(){
     cout << "Constructor Terpanggil" << endl; //definisi contructor
 };
+
+int main(){
+    mahasiswa mhs; //Pembuatan objek
+    return 0;
+}
